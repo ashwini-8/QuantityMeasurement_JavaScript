3e778c7a503313a0718b1b6479db093c0acbbd52
ashwini-8 <ashwinip309@gmail.com>
2021-01-22 10:43:52 +0530

    Added: TC 1.1 0 feet and 0 inch should return true

--- test/QuantityMeasurement.test.js
+++ test/QuantityMeasurement.test.js
@@ -1,6 +1,11 @@
 const feetToInchConverter = require("../main/QuantityMeasurement")
 
- test('providefilelocation_whenNumberOfRecodeMatch_TestShouldPass', () =>{
+ test('providefilelocation_whenNumberOfRecordsMatch_TestShouldPass', () =>{
     const isEqual = feetToInchConverter(1, 12);
+    return expect(isEqual).toBe(true); 
+});
+
+test('givenZeroInput_whenNumberOfRecordsMatch_TestShouldPass', () =>{
+    const isEqual = feetToInchConverter(0, 0);
     return expect(isEqual).toBe(true); 
 });
